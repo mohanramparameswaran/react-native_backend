1e4cd6f5035586e476f0700ff9e3a33fb32eed34
mohanramparameswaran <mohanramparameswaran@gmail.com>
2020-09-20 15:03:52 +0530

    Login feature

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 
 var app = express();
+
+const pg = require("pg");
+const R = require("ramda");
+
+const cs = "postgresql://postgres:Paramesh@1997@localhost:5433/postgres";
 var fs = require("fs");
 var bodyparser = require("body-parser");
 
@@ -16,13 +21,54 @@ app.use(express.json());
 // });
 
 app.post("/create_user", function (req, res) {
+  let result;
   console.log(req.body.lastName);
 
   const newUser = new User(req.body);
 
   newUser.save();
+  result = "success";
+  res.end(result);
+});
 
-  res.end("success");
+app.post("/fetch_data", function (req, res1) {
+  let result;
+  console.log("------------req.body-------------");
+  console.log(req.body);
+  const client = new pg.Client(cs);
+  client.connect();
+
+  client
+    .query("SELECT * FROM  users")
+    .then((res) => {
+      const data = res.rows;
+
+      data.forEach((row) => {
+        console.log(
+          `Id: ${row.id} Name: ${row.firstName} Lname: ${row.lastName}`
+        );
+        console.log("---------------Names---------------");
+        console.log(row.firstName);
+        console.log("---------------Req Names---------------");
+        console.log(req.body.firstName);
+
+        if (row.firstName == req.body.firstName) {
+          console.log("inside if");
+          result = "successful";
+          res1.send(result);
+        }
+      });
+      console.log("------------result--------------");
+      console.log(result);
+      if (result == "successful") {
+        console.log("Data in DB");
+      } else {
+        console.log("Not in DB");
+      }
+    })
+    .finally(() => {
+      client.end();
+    });
 });
 
 var server = app.listen(8000, function () {
